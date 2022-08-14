d705900c0b95f53e1024f65f868403ef04194805
ongweisheng <ongweisheng30@gmail.com>
2022-08-14 21:07:27 +0800

    Add playRound function

--- gameEngine.js
+++ gameEngine.js
@@ -1,6 +1,34 @@
-const CHOICES = ["Rock", "Paper", "Scissors"];
+const CHOICES = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let choice = CHOICES[Math.floor(Math.random() * 3)];
     return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock") {
+        if (computerSelection.toLowerCase() == "rock") {
+            return "Draw! Rock draws with Rock";
+        } else if (computerSelection.toLowerCase() == "paper") {
+            return "You Lose! Paper beats Rock";
+        } else if (computerSelection.toLowerCase() == "scissors") {
+            return "You Win! Rock beats Scissors"
+        }
+    } else if (playerSelection.toLowerCase() == "paper") {
+        if (computerSelection.toLowerCase() == "rock") {
+            return "You Win! Paper beats Rock";
+        } else if (computerSelection.toLowerCase() == "paper") {
+            return "Draw! Paper draws with Paper";
+        } else if (computerSelection.toLowerCase() == "scissors") {
+            return "You Lose! Scissors beats Paper"
+        }
+    } else if (playerSelection.toLowerCase() == "scissors") {
+        if (computerSelection.toLowerCase() == "rock") {
+            return "You Lose! Rock beats Scissors";
+        } else if (computerSelection.toLowerCase() == "paper") {
+            return "You Win! Scissors beats Paper";
+        } else if (computerSelection.toLowerCase() == "scissors") {
+            return "Draw! Scissors draws with Scissors"
+        }
+    }
 }
